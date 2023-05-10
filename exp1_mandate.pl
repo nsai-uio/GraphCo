801a228a85b4b74e96736f789251bb2536f8d99c
zhuoxun <zhengzx712@gmail.com>
2023-05-10 17:41:59 +0200

    init

--- exp1_mandate.pl
+++
@@ -1,2 +0,0 @@
-validTask(X) :- weightedSum(X), hasInput(X,Y).
-falseTaskWOInput(X) :- weightedSum(X), not validTask(X).
--- exp3_mandate.pl
+++
@@ -1,9 +0,0 @@
-concateArray(X):- concatenation(X), hasInput(X,Y), array(Y).
-concateSingleValue(X):- concatenation(X), hasInput(X,Y), singleValue(Y).
-
-concateArrayOnly(X):-concateArray(X), not concateSingleValue(X).
-concateSingleValueOnly(X):-concateSingleValue(X), not concateArray(X).
-
-falseConcateInputType(X) :- concatenation(X), not concateArrayOnly(X), not concateSingleValueOnly(X).
-
-
